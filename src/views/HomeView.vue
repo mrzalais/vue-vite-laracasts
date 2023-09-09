b09e92bf727ee11af8ab91d6c78664b6629071f9
mrzalais <lejnieksedijs@gmail.com>
2023-09-09 23:41:30 +0300

    Use a composable for code reusability

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { useFlash } from "@/composables/useFlash";
+
+let { flash } = useFlash();
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <p>
+      <button @click="flash('It works')">Click me</button>
+    </p>
   </main>
 </template>
