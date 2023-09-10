f3d3194ad2dc33856439c7a0c4fe1006a6cd3051
mrzalais <lejnieksedijs@gmail.com>
2023-09-10 17:52:25 +0300

    Use defineProps and defineEmits in script setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,14 @@
 <script setup>
-function onTabPress(event) {
-  let textarea = event.target
+import TabbableTextarea from '@/components/TabbableTextarea.vue'
+import { ref } from "vue";
 
-  let val = textarea.value,
-      start = textarea.selectionStart,
-      end = textarea.selectionEnd
-
-  textarea.value = val.substring(0, start) + "\t" + val.substring(end)
-
-  textarea.selectionStart = textarea.selectionEnd = start + 1
-}
-
+let comment = ref('test value')
 </script>
 
 <template>
   <main>
     <form>
-      <textarea ref="textarea" @keydown.tab.prevent="onTabPress" style="width: 100%; height: 300px;"></textarea>
+      <TabbableTextarea v-model="comment" style="width: 100%; height: 300px;"></TabbableTextarea>
     </form>
   </main>
 </template>
